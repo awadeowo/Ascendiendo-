7a5cffc4109418b0b7ea965699a83af249c5bf37
Joaquin Gonzalez <jie.gonzalezs@gmail.com>
2018-12-06 02:13:43 -0300

    Basura
    Home

--- Ascender/Ascensores/urls.py
+++ Ascender/Ascensores/urls.py
@@ -5,7 +5,7 @@ from Ascensores import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-url(r'^crearOrden$', views.OrdenCreate.as_view(), name='OrdenCreate'),
+    url(r'^crearOrden$', views.OrdenCreate.as_view(), name='OrdenCreate'),
     url(r'^listarOrden', views.OrdenList.as_view(), name='OrdenList'),
     url(r'^listarCliente', views.ClienteList.as_view(), name='ClienteList'),
     url(r'^CrearCliente', views.ClienteCreate.as_view(), name='ClienteCreate'),
